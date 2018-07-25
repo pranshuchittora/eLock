5d58faac163fffa674033640789be93d84506d60
pranshuchittora <pranshuchittora17@gmail.com>
2018-07-26 00:58:34 +0530

    added gh-pages URL

---
+++ public/main.js
@@ -0,0 +1,15 @@
+let redirectURL = "https://pranshuchittora.github.io/eLock/index.html";
+const onLine_message = document.getElementById('connection_status');
+if (navigator.onLine) {
+    onLine_message.innerHTML = "You are Online";
+    setInterval(() => { window.location = redirectURL }, 1000);
+}
+else {
+    onLine_message.innerHTML = "You are Offline"
+    setInterval(() => {
+        if (navigator.onLine)
+            window.location = redirectURL;
+    }, 1000);
+}
+
+console.log(navigator.onLine);
