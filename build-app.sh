a55e584cfb6a073fe2539fb300413dd3ffe6856c
pranshuchittora <pranshuchittora17@gmail.com>
2018-09-12 14:52:08 +0530

    Added zipping for all releases

--- build-app.sh
+++ build-app.sh
@@ -4,4 +4,6 @@ npm run build-win
 npm run build-linux
 npm run build-mac
 cd release-builds
+zip -r elock-win.zip eLock-win32-ia32
 zip -r elock-linux.zip eLock-linux-x64
+zip -r elock-mac.zip eLock-darwin-x64
--- get-source.sh
+++ get-source.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git clone https://github.com/pranshuchittora/eLock.git
+chmod 777 -R eLock 
 cd eLock
 npm install
