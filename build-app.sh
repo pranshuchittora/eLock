ec9a0c0509385300e33228efd02e757c0acf39ba
pranshuchittora <pranshuchittora17@gmail.com>
2018-09-12 14:24:52 +0530

    Added zip support

--- build-app.sh
+++ build-app.sh
@@ -3,3 +3,5 @@ rm -rf .git release-builds
 npm run build-win
 npm run build-linux
 npm run build-mac
+cd release-builds
+zip -r elock-linux.zip eLock-linux-x64
--- public/main.js
+++ public/main.js
@@ -1,15 +1,15 @@
-let redirectURL = "https://pranshuchittora.github.io/eLock/index.html";
-const onLine_message = document.getElementById('connection_status');
+let redirectURL = 'https://pranshuchittora.github.io/eLock/index.html'
+const onLine_message = document.getElementById('connection_status')
 if (navigator.onLine) {
-    onLine_message.innerHTML = "You are Online";
-    setInterval(() => { window.location = redirectURL }, 1000);
-}
-else {
-    onLine_message.innerHTML = "You are Offline"
+    onLine_message.innerHTML = 'You are Online'
     setInterval(() => {
-        if (navigator.onLine)
-            window.location = redirectURL;
-    }, 1000);
+        window.location = redirectURL
+    }, 1000)
+} else {
+    onLine_message.innerHTML = 'You are Offline'
+    setInterval(() => {
+        if (navigator.onLine) window.location = redirectURL
+    }, 1000)
 }
 
-console.log(navigator.onLine);
+console.log(navigator.onLine)
